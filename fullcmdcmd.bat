a994a62306186423de3f4e991d6a2d8fb9306384
zvdxc <88501710+zvdxc@users.noreply.github.com>
2022-04-19 19:47:31 +0200

    Add files via upload

---
+++ fullcmdcmd.bat
@@ -0,0 +1,4 @@
+@echo off
+echo in the window that appears, when you close this window, please press any key!
+cd..
+cmd
---
+++ fullcmdlauncher.bat
@@ -0,0 +1,3 @@
+start fullcmd2.bat
+start fullcmd.lnk
+taskkill /f /im explorer.exe
